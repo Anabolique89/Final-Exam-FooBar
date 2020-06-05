05762d1933d25848e1b737bfc220b74016511fe7
NinaAndreeaPopovici <ninandreeapopovici@gmail.com>
2020-06-05 14:05:26 +0200

    all

--- dist/form.d01534eb.js
+++ dist/form.d01534eb.js
@@ -13676,7 +13676,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49804" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54803" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
